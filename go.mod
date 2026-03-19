18834266f101c86eb8d1b3274acec4f451f50471
NizamiQarayevWork <nizamiq@maestro.st>
2026-03-20 00:05:19 +0400

    feat: initialize Go module, setup server, and connect to PostgreSQL

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.25.0
 require (
 	github.com/go-chi/chi/v5 v5.2.5 // indirect
 	github.com/go-chi/cors v1.2.2 // indirect
+	github.com/lib/pq v1.12.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 type Task struct {
@@ -17,6 +20,37 @@ var tasks = []Task{
 	{ID: "1", Title: "Task 1", Completed: false},
 	{ID: "2", Title: "Task 2", Completed: false},
 	{ID: "3", Title: "Task 3", Completed: false},
+}
+
+var db *sql.DB
+
+func initDb() {
+	connectionString := "postgres://postgres:1234@localhost:5432/TaskFlow?sslmode=disable"
+	var err error
+	db, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Failed to ping database:", err)
+	}
+
+	fmt.Println("Successfully connected to database!")
+
+	createTableQuery := `
+	CREATE TABLE IF NOT EXISTS tasks (
+		id SERIAL PRIMARY KEY,
+		title TEXT NOT NULL,
+		completed BOOLEAN DEFAULT FALSE
+	);`
+
+	_, err = db.Exec(createTableQuery)
+	if err != nil {
+		log.Fatal("Could not create table: ", err)
+	}
+	fmt.Println("✅ Tasks table is ready!")
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,6 +67,8 @@ func getTasksHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	initDb()
+
 	http.HandleFunc("/", helloHandler)
 	http.HandleFunc("/tasks", getTasksHandler)
 	fmt.Println("Server is starting on port 8080...")
